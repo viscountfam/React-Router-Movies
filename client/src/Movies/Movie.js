4e43f4060bf464481beec16f92fe18850265471e
viscountfam <V3ctor42!>
2019-12-17 18:08:22 -0500

    One stretch goal complete

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import {useParams} from 'react-router-dom';
+import MovieCard from './MovieCard';
 const Movie = (props) => {
   const [movie, setMovie] = useState();
   const {id} = useParams();
@@ -33,22 +34,7 @@ const Movie = (props) => {
   const { title, director, metascore, stars } = movie;
   return (
     <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
+      <MovieCard className="movie-card" title={title} director={director} metascore={metascore} stars={stars} />
       <div  onClick="" className="save-button">Save</div>
     </div>
   );
